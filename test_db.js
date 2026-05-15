17a75796c719f0edf6cae9257a0cab47ea6e65b9
Developer <developer@example.com>
2026-05-15 15:34:00 +0700

    Add test_db.js for database testing

---
+++ test_db.js
@@ -0,0 +1,14 @@
+const sequelize = require('./config/db');
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    process.exit(0);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1);
+  }
+}
+
+testConnection();
